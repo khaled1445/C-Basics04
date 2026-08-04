c0bb7f64413a604e44737c77c0e8940cc7e40ed9
5aled998 <khaledamr142005@.com>
2026-08-04 13:06:12 +0300

    ass_3 Problem_3

--- C#Basics04/Program.cs
+++ C#Basics04/Program.cs
@@ -28,6 +28,15 @@ namespace C_Basics04
             //}
             #endregion
 
+            #region Problem 3
+            ////.Create a StringBuilder, Append() the text "Book List",
+            ////then Append() " - Updated" onto the same object. Print the final result
+            //StringBuilder sb01 = new StringBuilder();
+            //sb01.Append("Book List");
+            //sb01.Append(" - Updated");
+            //Console.WriteLine(sb01);
+            #endregion
+
         }
     }
 }
