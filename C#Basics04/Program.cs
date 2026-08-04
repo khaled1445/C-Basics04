efa931cb7744accec48f69ef0be0f2a1965eed62
5aled998 <khaledamr142005@.com>
2026-08-04 08:18:17 +0300

    ass_3 Problem_1

--- C#Basics04/Program.cs
+++ C#Basics04/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // the rest of Assignment 3 
+            #region Problem 1
+            //.Declare string title = "clean code";. Call title.ToUpper() and store it in a new variable upperTitle. 
+            //Print both title and upperTitle to show that title did not change.
+            string title = "clean code";
+            string upperTitle = title.ToUpper();
+            Console.WriteLine(title);
+            Console.WriteLine(upperTitle);
+            #endregion
+
         }
     }
 }
