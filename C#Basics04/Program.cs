51d01d4a07bb3e16791fa5dc93c6d0f2a1a522e4
5aled998 <khaledamr142005@.com>
2026-08-04 08:19:00 +0300

    ass_3 Problem_1

--- C#Basics04/Program.cs
+++ C#Basics04/Program.cs
@@ -6,12 +6,12 @@
         {
             // the rest of Assignment 3 
             #region Problem 1
-            //.Declare string title = "clean code";. Call title.ToUpper() and store it in a new variable upperTitle. 
-            //Print both title and upperTitle to show that title did not change.
-            string title = "clean code";
-            string upperTitle = title.ToUpper();
-            Console.WriteLine(title);
-            Console.WriteLine(upperTitle);
+            ////.Declare string title = "clean code";. Call title.ToUpper() and store it in a new variable upperTitle. 
+            ////Print both title and upperTitle to show that title did not change.
+            //string title = "clean code";
+            //string upperTitle = title.ToUpper();
+            //Console.WriteLine(title);
+            //Console.WriteLine(upperTitle);
             #endregion
 
         }
