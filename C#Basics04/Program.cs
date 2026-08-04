5944b86126576ce65ccbfdb3b96f52f9935ce9a8
5aled998 <khaledamr142005@.com>
2026-08-04 13:25:22 +0300

    Problem_1

--- C#Basics04/Program.cs
+++ C#Basics04/Program.cs
@@ -88,6 +88,14 @@ namespace C_Basics04
             //Console.WriteLine(PrintFirstBook(books));
 
             #endregion
+
+            //Assignment04
+
+            #region Problem 1
+            ////Create a one-dimensional array double[] prices with the values 25.5, 40.0, 33.75. Print the second price (index 1).
+            //double[] prices = { 25.5, 40.0, 33.75 };
+            //Console.WriteLine(prices[1]);
+            #endregion
         }
     }
 }
