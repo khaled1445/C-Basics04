e004105fa2465bf43b87a17bb5153fa9b1784008
5aled998 <khaledamr142005@.com>
2026-08-04 12:57:28 +0300

    ass_3 Problem_1

--- C#Basics04/Program.cs
+++ C#Basics04/Program.cs
@@ -16,37 +16,7 @@ namespace C_Basics04
             //Console.WriteLine(upperTitle);
             #endregion
 
-            #region Problem 2
-            //Declare two separate string variables, both set to the literal "Clean Code".
-            //Use ReferenceEquals() to check if they point to the same object in memory.
 
-            //string str1 = "Clean Code";
-            //string str2 = "Clean Code";
-            //if (ReferenceEquals(str1, str2)) 
-            //{ 
-            //Console.WriteLine("string 1 and string 2 refers to the same object: " + ReferenceEquals(str1, str2));
-            //}
-            #endregion
-
-            #region Problem 3
-            ////.Create a StringBuilder, Append() the text "Book List",
-            ////then Append() " - Updated" onto the same object. Print the final result
-            //StringBuilder sb01 = new StringBuilder();
-            //sb01.Append("Book List");
-            //sb01.Append(" - Updated");
-            //Console.WriteLine(sb01);
-            #endregion
-
-            #region Problem 4
-            ////Using the StringBuilder from the question above,use Replace() to change "Book List" into "Library".Print the result
-            //StringBuilder sb01 = new StringBuilder();
-            //sb01.Append("Book List");
-            //sb01.Append(" - Updated");
-            //sb01.Replace("Book List","Library");
-            //Console.WriteLine(sb01);
-            #endregion
-
-          
         }
     }
 }
